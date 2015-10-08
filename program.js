9418e178cd767a3af048515ab4336eb9c76c3121
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-10-07 22:07:56 -0400

    add code for challenge 5, stylus

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
-// app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+app.set('views', process.argv[3]);
+app.set('stylesheets', process.argv[3]);
 
 app.get('/home', function(req, res) {
     // res.send('Hello World!');
@@ -18,6 +19,11 @@ app.use(bodyparser.urlencoded({
 app.post('/form', function(req, res) {
     res.end(req.body.str.split('').reverse().join(''));
 });
+
+var stylus = require('stylus');
+app.use(stylus.middleware(process.argv[3]));
+// app.use('/main.css', express.static(process.argv[3] + '/main.css'));
+app.use(express.static(process.argv[3]));
 
 // app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 app.listen(process.argv[2]);
