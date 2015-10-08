9b083661a3f351647afcc02966876623e5227c69
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-10-07 20:53:56 -0400

    add code for challenge 3, render index

--- program.js
+++ program.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
-app.get('/home', function(req,res) {
-  res.send('Hello World!');
+app.set('view engine', 'jade');
+app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+
+app.get('/home', function(req, res) {
+    // res.send('Hello World!');
+    res.render('index', {
+        date: new Date().toDateString()
+    });
 });
-app.use(express.static(process.argv[3] || path.join(__dirname,'public')));
+
+// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 app.listen(process.argv[2]);
