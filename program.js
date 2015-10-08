50ef2d10be60b7aa1456936e84e98892ced1fac0
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-10-07 21:08:04 -0400

    add code for challenge 4, reverse string

--- program.js
+++ program.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
-app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+// app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
 
 app.get('/home', function(req, res) {
     // res.send('Hello World!');
     res.render('index', {
         date: new Date().toDateString()
     });
+});
+
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
+
+app.post('/form', function(req, res) {
+    res.end(req.body.str.split('').reverse().join(''));
 });
 
 // app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
